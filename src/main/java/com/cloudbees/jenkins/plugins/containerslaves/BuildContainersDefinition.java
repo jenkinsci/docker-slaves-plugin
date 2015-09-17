809dba29762084e2fba996e9d93dcedf51e1f45b
Nicolas De Loof <nicolas.deloof@gmail.com>
2015-09-17 16:20:01 +0200

    UI to define containers for a build

---
+++ src/main/java/com/cloudbees/jenkins/plugins/containerslaves/BuildContainersDefinition.java
@@ -0,0 +1,73 @@
+/*
+ * The MIT License
+ *
+ *  Copyright (c) 2015, CloudBees, Inc.
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ *
+ */
+
+package com.cloudbees.jenkins.plugins.containerslaves;
+
+import hudson.Extension;
+import hudson.model.Job;
+import hudson.model.JobProperty;
+import hudson.model.JobPropertyDescriptor;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+import java.util.List;
+
+/**
+ * Definition for a set of containers to host the build.
+ * @author <a href="mailto:nicolas.deloof@gmail.com">Nicolas De Loof</a>
+ */
+public class BuildContainersDefinition extends JobProperty {
+
+    private final String buildHostImage;
+
+    private final List<SideContainerDefinition> sideContainers;
+
+    @DataBoundConstructor
+    public BuildContainersDefinition(String buildHostImage, List<SideContainerDefinition> sideContainers) {
+        this.buildHostImage = buildHostImage;
+        this.sideContainers = sideContainers;
+    }
+
+    public String getBuildHostImage() {
+        return buildHostImage;
+    }
+
+    public List<SideContainerDefinition> getSideContainers() {
+        return sideContainers;
+    }
+
+    @Extension
+    public static class DescriptorImpl extends JobPropertyDescriptor {
+
+        @Override
+        public boolean isApplicable(Class<? extends Job> jobType) {
+            return true;
+        }
+
+        @Override
+        public String getDisplayName() {
+            return "Containers to host the build";
+        }
+    }
+}
